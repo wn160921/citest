386c470399c117de135a31b0bc2c4d1c314792ac
王宁 <2556405022@qq.com>
2019-03-29 22:31:24 +0800

    测试部分部署test

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,5 +4,5 @@
 #cd dist
 #mv web/* ././../public/
 #cd ../../
-sbt dist
+sbt dist exit
 sshpass -p Wang1234561s!! scp -P 22 -o stricthostkeychecking=no -r target/universal/citest-1.0.0-SNAPSHOT.zip root@106.13.54.28:/root/
