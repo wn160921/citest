143909da0fe7adfefed9639830264d4d901c7b7f
王宁 <2556405022@qq.com>
2019-03-29 21:54:15 +0800

    测试部分部署

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/env bash
-apt install sshpass
-cd web
-ng build --prod
-cd dist
-mv web/* ././../public/
-cd ../../
+#cd web
+#ng build --prod
+#cd dist
+#mv web/* ././../public/
+#cd ../../
 sbt dist
 sshpass -p Wang1234561s!! scp -P 22 -o stricthostkeychecking=no -r target/universal/citest-1.0.0-SNAPSHOT.zip root@106.13.54.28:/root/
